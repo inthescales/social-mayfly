923d6602d9636a6a230a0d7edf5cc565e7efda03
Robin <rehill012@gmail.com>
2024-11-07 21:27:33 -0500

    Add command line args

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Social Mayfly (WIP)
 
-Tool for automatically deleting older social media content.
+Tool for automatically deleting old social media content.
 
-This is a work in progress, and does not function yet.
+This is a non-functional work in progress, and should not be used.
--- social-mayfly.py
+++ social-mayfly.py
@@ -1,15 +1,39 @@
+import getopt
+import sys
+
 import src.credentialing as credentialing
 
 from src.bluesky import Bluesky_Client
 from src.mastodon import Mastodon_Client
 
-credentials = credentialing.read_creds("creds.json")
-threshold = 30
+def run(creds_file, threshold):
+	credentials = credentialing.read_creds(creds_file)
 
-for cred in credentials:
-	if cred["type"] == "bluesky":
-		client = Bluesky_Client(cred["handle"], cred["password"])
-	elif cred["type"] == "mastodon":
-		client = Mastodon_Client(cred["access_token"], cred["api_base_url"])
+	for cred in credentials:
+		if cred["type"] == "bluesky":
+			client = Bluesky_Client(cred["handle"], cred["password"])
+		elif cred["type"] == "mastodon":
+			client = Mastodon_Client(cred["access_token"], cred["api_base_url"])
 
-	client.delete_posts_after_threshold(threshold)
+		client.delete_posts_after_threshold(threshold)
+
+if __name__ == '__main__' and len(sys.argv) > 0:
+
+	creds_file = "creds.json"
+	threshold = 30
+
+	try:
+		opts, params = getopt.getopt(sys.argv[1:], "c:t:", ["creds", "threshold"])
+	except getopt.GetoptError:
+		print('ERROR: getopt error')
+		sys.exit(2)
+
+	# Process args
+	for opt, arg in opts:
+		if opt in ["-c", "--creds"]:
+			creds_file = arg
+		elif opt in ["-t", "--threshold"]:
+			threshold = int(arg)
+
+	run(creds_file, threshold)
+    
